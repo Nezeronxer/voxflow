f35a823989a7708340ed769fc31935f57b2ca27c
Nezeronxer <nezeronxer995@gmail.com>
2026-08-16 22:08:12 +0300

    VoxFlow 2.0.17: постобработка на любом сервисе и любом ключе, Fn как клавиша диктовки

--- voxflow/src/sections/SettingsHub.tsx
+++ voxflow/src/sections/SettingsHub.tsx
@@ -92,7 +92,7 @@ export default function SettingsHub({
           <Icon.Check className="ico" />
           Локальный режим · данные не покидают устройство
         </span>
-        <span>VoxFlow 2.0.16</span>
+        <span>VoxFlow 2.0.17</span>
       </footer>
     </div>
   );
